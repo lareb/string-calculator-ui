513f9328d654c4c720b6e087e51edf71a41f2e75
Lareb Nawab <lareb.indore@gmail.com>
2025-03-30 15:35:33 +0530

    test: Check if input field updates on user typing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { fireEvent } from "@testing-library/react";
 
 test("renders String Calculator heading", () => {
   render(<App />);
@@ -10,4 +11,11 @@ test("renders input and button", () => {
   render(<App />);
   expect(screen.getByPlaceholderText(/Enter numbers/i)).toBeInTheDocument();
   expect(screen.getByText("Calculate")).toBeInTheDocument();
+});
+
+test("updates input field on user typing", () => {
+  render(<App />);
+  const textarea = screen.getByPlaceholderText(/Enter numbers/i);
+  fireEvent.change(textarea, { target: { value: "1,2,3" } });
+  expect(textarea.value).toBe("1,2,3");
 });
